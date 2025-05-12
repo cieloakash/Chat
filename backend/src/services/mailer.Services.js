d9f0c5a600da98e7f1439e6dcb75d8c62ae0b02e
Sky <akpakash490@gmail.com>
2025-05-13 00:38:06 +0530

    final commit

--- backend/src/services/mailer.Services.js
+++ backend/src/services/mailer.Services.js
@@ -15,9 +15,9 @@ export const otpServices = {
   sendOtp: async (email) => {
     try {
       await OTP.deleteMany({ email });
-      // 6digit otp
-      const otp = 112233
-      // const otp = Math.floor(100000 + Math.random() * 900000).toString();
+      // 8digit otp
+
+      const otp = Math.floor(100000 + Math.random() * 900000).toString();
       const expiresAt = new Date(Date.now() + 5 * 60 * 1000); // 5 minutes expiration
 
       await OTP.create({
