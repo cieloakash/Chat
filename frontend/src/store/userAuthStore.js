68807f02f19af49697ed6656aecdc19df2a8661a
Sky <akpakash490@gmail.com>
2025-05-30 11:54:30 +0530

    error in middleware

--- frontend/src/store/userAuthStore.js
+++ frontend/src/store/userAuthStore.js
@@ -3,7 +3,7 @@ import { axiosInstance } from "../lib/axios.js";
 import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
-const BASE_URL = import.meta.env.VITE_BACKEND_CORS;
+const BASE_URL =import.meta.env.VITE_BACKEND_CORS_SOCKET;
 
 export const userAuthStore = create((set, get) => ({
   authUser: null,
