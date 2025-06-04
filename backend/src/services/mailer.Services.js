fba3a9f3b29898903477645d37378ed87d72a65f
Sky <akpakash490@gmail.com>
2025-06-04 16:54:01 +0530

    otp send service update

--- backend/src/services/mailer.Services.js
+++ backend/src/services/mailer.Services.js
@@ -16,8 +16,7 @@ export const otpServices = {
     try {
       await OTP.deleteMany({ email });
       // 6digit otp
-      const otp = 112233
-      // const otp = Math.floor(100000 + Math.random() * 900000).toString();
+      const otp = Math.floor(100000 + Math.random() * 900000).toString();
       const expiresAt = new Date(Date.now() + 5 * 60 * 1000); // 5 minutes expiration
 
       await OTP.create({
